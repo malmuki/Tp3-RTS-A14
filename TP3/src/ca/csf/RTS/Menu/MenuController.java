afb0057f936ca39614d641256c4b4b00022dfb40
Francois Chantal <geniousmouton@gmail.com>
2014-11-18 15:28:28 -0500

    enlever lwjgl mit jsfml

--- TP3/src/ca/csf/RTS/Menu/MenuController.java
+++ TP3/src/ca/csf/RTS/Menu/MenuController.java
@@ -1,20 +1,17 @@
 package ca.csf.RTS.Menu;
 
-import ca.csf.RTS.Game.Screen;
 import ca.csf.simpleFx.SimpleFXController;
 import javafx.fxml.FXML;
 
 public class MenuController extends SimpleFXController {
 
-	private Screen screen;
 	
 	public MenuController() {
-		screen = new Screen();
+
 	}
 	
 	@FXML
 	public void startGame() {
-		screen.execute();
 		getSimpleFxStage().close();
 	}
 
