b23cd7306e701898e28eb916390af9aa84726861
Francois Chantal <geniousmouton@gmail.com>
2014-11-28 12:44:59 -0500

    plus de change

---
+++ TP3/src/ca/csf/RTS/game/entity/state/Training.java
@@ -0,0 +1,13 @@
+package ca.csf.RTS.game.entity.state;
+
+import ca.csf.RTS.game.entity.GameEntity;
+
+public class Training implements State {
+
+	@Override
+	public void action(GameEntity gameEntity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
