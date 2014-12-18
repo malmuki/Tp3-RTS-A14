c106a9aa2c9903f485ccdc93af64732a1feb7289
Francois Chantal <geniousmouton@gmail.com>
2014-12-18 11:54:13 -0500

    flush

--- TP3/src/ca/csf/RTS/entity/concrete/state/State.java
+++
@@ -1,8 +0,0 @@
-package ca.csf.RTS.entity.concrete.state;
-
-import ca.csf.RTS.entity.Entity;
-
-public interface State {
-	
-	public void action(Entity entity);
-}
--- TP3/src/ca/csf/RTS/eventHandler/MenuEventHandler.java
+++
@@ -1,5 +0,0 @@
-package ca.csf.RTS.eventHandler;
-
-public interface MenuEventHandler {
-
-}
