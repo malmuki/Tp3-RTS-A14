7f64bc83dd769222fc638ec1d64e4405abef809b
Samuel <samuel.theriault.hall@gmail.com>
2014-12-18 10:49:52 -0500

    Replaced hard-coded strings with constants

--- TP3/src/ca/csf/RTS/game/entity/controllableEntity/human/Worker.java
+++ TP3/src/ca/csf/RTS/game/entity/controllableEntity/human/Worker.java
@@ -17,81 +17,83 @@ import ca.csf.RTS.game.pathFinding.PathFinder;
 
 public class Worker extends Human {
 
-	private Texture texture;
-	private static final int MAX_HEALTH = 100;
-	private static final String NAME = "Worker";
+  private static String TEXTURE_PATH = "./ressource/worker.gif";
+  private Texture texture;
+  private static final int MAX_HEALTH = 100;
+  private static final String NAME = "Worker";
+  private static final int RESSOURCE_SEARCH_RANGE = 35; // this accounts for 10 per horizontal move and 14 for diagonal
 
-	public Worker(Tile originTile, Team team, GameEventHandler game) {
-		super(originTile, MAX_HEALTH, team, game);
-		try {
-			if (texture == null) {
-				texture = new Texture();
-				texture.loadFromFile(Paths.get("./ressource/worker.gif"));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		sprite.setTexture(texture);
-		setSpritePos();
-	}
+  public Worker(Tile originTile, Team team, GameEventHandler game) {
+    super(originTile, MAX_HEALTH, team, game);
+    try {
+      if (texture == null) {
+        texture = new Texture();
+        texture.loadFromFile(Paths.get(TEXTURE_PATH));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    sprite.setTexture(texture);
+    setSpritePos();
+  }
 
-	@Override
-	public State getDefaultState() {
-		return new Idle();
-	}
+  @Override
+  public State getDefaultState() {
+    return new Idle();
+  }
 
-	@Override
-	public void order(Entity target) {
-		stateStack.clear();
-		if (target.getTeam().getName() == "Nature") {
-			setTarget(target);
-			stateStack.push(new Gathering(this));
-		} else {
-			stateStack.push(new Move(target.getTilesOrigin(), this));
-		}
-	}
+  @Override
+  public void order(Entity target) {
+    stateStack.clear();
+    if (target.getTeam().getName() == "Nature") {
+      setTarget(target);
+      stateStack.push(new Gathering(this));
+    } else {
+      stateStack.push(new Move(target.getTilesOrigin(), this));
+    }
+  }
 
-	@Override
-	public void doTasks(float deltaTime) {
-		if (!stateStack.isEmpty()) {
-			switch (stateStack.peek().action(deltaTime)) {
-			case ended:
-				stateStack.pop();
-				if (stateStack.isEmpty()) {
-					if (target != null) {
-						stateStack.push(new Gathering(this));
-					}else {
-						stateStack.push(getDefaultState());
-					}
-				}
-				
-				break;
-			case targetTooFar:
-				stateStack.push(new Move(target.getTilesOrigin(), this));
-				break;
-			case targetUnreachable:
+  @Override
+  public void doTasks(float deltaTime) {
+    if (!stateStack.isEmpty()) {
+      switch (stateStack.peek().action(deltaTime)) {
+        case ended:
+          stateStack.pop();
+          if (stateStack.isEmpty()) {
+            if (target != null) {
+              stateStack.push(new Gathering(this));
+            } else {
+              stateStack.push(getDefaultState());
+            }
+          }
 
-				break;
-			case ressourceDepleted:
-				stateStack.pop();
-				setTarget(search());
-				break;
-			case dead:
-				game.remove(this);
-				break;
-			default:
-				break;
-			}
-		}
-	}
+          break;
+        case targetTooFar:
+          stateStack.push(new Move(target.getTilesOrigin(), this));
+          break;
+        case targetUnreachable:
 
-	@Override
-	public Entity search() {
-		return PathFinder.findClosestRessource(this, 150);
-	}
+          break;
+        case ressourceDepleted:
+          stateStack.pop();
+          setTarget(search());
+          break;
+        case dead:
+          game.remove(this);
+          break;
+        default:
+          break;
+      }
+    }
+  }
 
-	@Override
-	public String getName() {
-		return NAME;
-	}
+  @Override
+  public Entity search() {
+    return PathFinder.findClosestRessource(this, RESSOURCE_SEARCH_RANGE);
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
 }
